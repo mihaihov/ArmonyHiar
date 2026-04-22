aee7fad5160d5b78c19a7e3436ba9e533aaa95ce
Mihai <raducumihaicristian@gmail.com>
2026-04-22 12:40:52 +0000

    fix: adjust Mirel's photo alignment to object-top

--- src/components/TeamPage/TeamPage.jsx
+++ src/components/TeamPage/TeamPage.jsx
@@ -8,7 +8,7 @@ import { logoWhite } from '../../assets';
 const TeamMember = ({ name, src, link }) => {
     return (
         <div className="flex flex-col bg-white rounded-xl items-center py-4 my-4 shadow-white shadow-md ">
-            <img src={src} className={`rounded-full w-[350px] h-[350px] object-cover ${name === 'Alexandru' || name === 'George' || name === 'Vasi' ? 'object-top' : 'object-bottom'}`} alt={`Barber ${name} portrait`} />
+            <img src={src} className={`rounded-full w-[350px] h-[350px] object-cover ${name === 'Alexandru' || name === 'George' || name === 'Vasi' || name === 'Mirel' ? 'object-top' : 'object-bottom'}`} alt={`Barber ${name} portrait`} />
             <h1 className='mt-16 text-3xl tracking-wide font-poppins'>{name}</h1>
             <div>
                 <button className={`${styles.buttonTertiary}`}><a href={link} target='_blank'>Fa o programare</a></button>
